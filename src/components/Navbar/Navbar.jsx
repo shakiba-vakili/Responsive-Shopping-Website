a9a3b7111bbe0ed0fdf5cb14e87020115b000d6d
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2024-04-06 19:24:16 +0330

    Add shopping cart icon and update search functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { FaCartShopping } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
 const MenuLinks = [
   { id: 1, name: "Home", link: "/#" },
@@ -44,9 +45,13 @@ const Navbar = () => {
               <input type="text" placeholder="Search" className="search-bar" />
               <IoMdSearch className="text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 group-hover:text-primary duration-200" />
             </div>
+            {/* Order button section */}
+            <button className="relative p-3">
+              <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400 " />
+              <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">4</div>
+            </button>
             {/* Dark mode section */}
             <div></div>
-
           </div>
         </div>
       </div>
