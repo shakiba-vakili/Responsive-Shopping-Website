780a0258409a940eff97630c57e72308ef31659e
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2024-04-06 19:16:55 +0330

    Add search functionality and update menu links

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import { IoMdSearch } from "react-icons/io";
+const MenuLinks = [
+  { id: 1, name: "Home", link: "/#" },
+  { id: 2, name: "Shop", link: "/#shop" },
+  { id: 3, name: "About", link: "/#about" },
+  { id: 4, name: "Blogs", link: "/#blog" },
+];
+
+const Navbar = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="py-4">
+        <div className="container flex justify-between items-center">
+          {/* Logo and links */}
+          <div className="flex items-center gap-4">
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+            >
+              Eshop
+            </a>
+            {/* Menu item */}
+            {/*  */}
+            <div className="hidden lg:block">
+              <ul className="flex items-center gap-4">
+                {MenuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* Navbar right section */}
+
+          <div className="flex justify-between items-center gap-4">
+            {/* Search Bar section */}
+            <div className="relative group hidden sm:block">
+              <input type="text" placeholder="Search" className="search-bar" />
+              <IoMdSearch className="text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 group-hover:text-primary duration-200" />
+            </div>
+            {/* Dark mode section */}
+            <div></div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
