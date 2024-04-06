3f3967b66ed72911c36e9e518f39275b75c61c7b
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2024-04-06 20:04:12 +0330

    Add dark mode functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FaCartShopping } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
+import DarkMode from "./DarkMode";
 const MenuLinks = [
   { id: 1, name: "Home", link: "/#" },
   { id: 2, name: "Shop", link: "/#shop" },
@@ -51,7 +52,9 @@ const Navbar = () => {
               <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">4</div>
             </button>
             {/* Dark mode section */}
-            <div></div>
+            <div>
+              <DarkMode/>
+            </div>
           </div>
         </div>
       </div>
