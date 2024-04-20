fce9dd950b19802227a6629e889b7a486dcf30e2
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2024-04-20 18:37:17 +0330

    Add Button component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import Slider from "react-slick";
 import Image1 from "../../assets/hero/headphone.png";
 import Image2 from "../../assets/category/vr.png";
 import Image3 from "../../assets/category/macbook.png";
+import Button from "../../components/Shared/Button";
 const HeroData = [
   {
     id: 1,
@@ -54,9 +55,15 @@ const Hero = () => {
                     <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
                       {data.title}
                     </h1>
-                    <h1 className="text-5xl uppercase text-white dark:text-white/5  sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">{data.title2}</h1>
+                    <h1 className="text-5xl uppercase text-white dark:text-white/5  sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">
+                      {data.title2}
+                    </h1>
                     <div className="">
-                      <button>Shop Now</button>
+                      <Button
+                        text="Shop By Category"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                      />
                     </div>
                   </div>
                   <div className="sm:order-2 order-1">
@@ -64,7 +71,7 @@ const Hero = () => {
                       <img
                         src={data.img}
                         alt=""
-                        className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
+                        className="w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
                       />
                     </div>
                   </div>
@@ -77,5 +84,5 @@ const Hero = () => {
     </div>
   );
 };
-
+//1:22
 export default Hero;
