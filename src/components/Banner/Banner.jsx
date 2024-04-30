7b3e94b0e282225e3d613da3e1456f3a05d0fd5f
Shakiba Vakili <shakibvakili@gmail.com>
2024-04-30 18:05:59 +0330

    Implemented Banner Component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Banner = ({ data }) => {
+  return (
+    <div className=" min-h-[550px] flex justify-center items-center py-12">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl">
+          {/* first col */}
+          <div style={{ backgroundColor: data.bgColor }} className="p-6 sm:p-8">
+            <p className="text-sm"> {data.discount}</p>
+            <h1 className="uppercase text-4x large:text-7xl font-bold ">
+              {data.title}
+            </h1>
+            <p className="text-sm">{data.date}</p>
+          </div>
+          {/* second col */}
+          <div className="h-full flex items-center">
+            <img
+              src={data.img}
+              alt="banner image"
+              className="scale-125 w-[250px] md:w-[340px] mx-auto drop-shadow-2xl object-cover "
+            />
+          </div>
+          {/* third  col */}
+          <div className="flex flex-col justify-center gap-4 p-6 sm:p-8">
+            <p>{data.title2}</p>
+            <p>{data.title3}</p>
+            <p>{data.title4}</p>
+            <div className="">
+              <button>Shop Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
