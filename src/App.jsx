1a8fc164bf41641326a05d60bcd9fe0225a28b64
Shakiba Vakili <shakibvakili@gmail.com>
2024-04-30 17:27:17 +0330

    Implemented Banner component for displaying promotional content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
-import Hero from './components/Hero/Hero'
+import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
-import Services from './components/Services/Services'
+import Services from "./components/Services/Services";
+import Banner from './components/Banner/Banner'
 const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar />
-      <Hero/>
-      <Category/>
-      <Category2/>
-      <Services/>
-
+      <Hero />
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner/>
     </div>
   );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -35,14 +35,14 @@ const serviceData = [
 const Services = () => {
   return (
     <div>
-      <div className="container">
+      <div className="container my-14 md:my-20">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
           {serviceData.map((data) => (
             <div className="flex flex-col items-start sm:flex-row gap-4">
               {data.icon}
               <div className="">
-                <h1>{data.title}</h1>
-                <h1>{data.description}</h1>
+                <h1 className="lg:text-xl font-bold">{data.title}</h1>
+                <h1 className="text-gray-400 text-sm">{data.description}</h1>
               </div>
             </div>
           ))}
